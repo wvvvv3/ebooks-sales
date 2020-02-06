1cea48109b964b6d8676c89a12836d7397270461
wangyuwei <wangyuwei@bilibili.com>
2020-02-06 23:04:14 +0800

    引入iview

--- ebook/src/page/login.vue
+++ ebook/src/page/login.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     登录
+    <Radio :checked.sync="single">Radio</Radio>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      single: false
+    }
+  }
 }
 </script>
